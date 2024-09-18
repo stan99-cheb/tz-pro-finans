12e328ae23e915b51ad3bc96a07bdb6317eddab4
stan99 <stan99-cheb@yandex.ru>
2024-09-18 16:34:56 +0300

    fix: небольшие изменения типов

--- src/page/Content/content.tsx
+++ src/page/Content/content.tsx
@@ -15,10 +15,16 @@ export type TData = {
   price: number;
 };
 
+type SearchQuery = {
+  barcode: string;
+  product_brand: string;
+  product_name: string;
+};
+
 const Content = () => {
   const [data, setData] = React.useState<TData[]>(Data);
   const [selectedSort, setSelectedsort] = React.useState('barcode');
-  const [searchQuery, setSearchQuery] = React.useState({
+  const [searchQuery, setSearchQuery] = React.useState<SearchQuery>({
     barcode: '',
     product_brand: '',
     product_name: '',
@@ -26,17 +32,18 @@ const Content = () => {
 
   const sortedData = React.useMemo(
     () =>
-      [...data].sort((a, b) => a[selectedSort] > b[selectedSort] ? 1 : -1),
+      [...data].sort((a, b) => a[(selectedSort as keyof TData)] > b[selectedSort as keyof TData] ? 1 : -1),
     [data, selectedSort]
   );
 
   const sortedAndQueryData = React.useMemo(
     () =>
-      sortedData.filter(item => {
-        return Object.keys(searchQuery).every(key => {
-          return item[key].toString().includes(searchQuery[key]);
-        });
-      }),
+      sortedData.filter(item =>
+        (Object.keys(searchQuery) as (keyof SearchQuery)[]).every(
+          key =>
+            item[key].toString().includes(searchQuery[key])
+        )
+      ),
     [searchQuery, sortedData]
   );
 
